3fd21b991728e40698dae6a191fb5f4bda124449
Elson Reis <elsonreis084@gmail.com>
2026-04-17 20:54:49 -0300

    chore: initial project setup

---
+++ src/main/java/fortfrut/FortfrutApplication.java
@@ -0,0 +1,13 @@
+package fortfrut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortfrutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortfrutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fortfrut/FortfrutApplicationTests.java
@@ -0,0 +1,13 @@
+package fortfrut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortfrutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
