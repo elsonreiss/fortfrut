9dd8522085111695a3f1a2796a4f5123e297d7ad
Elson Reis <elsonreis084@gmail.com>
2026-04-17 21:14:45 -0300

    feat(product): create entity

---
+++ src/main/java/fortfrut/entity/Product.java
@@ -0,0 +1,40 @@
+package fortfrut.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String quantity;
+
+    @Column(nullable = false)
+    private String price;
+
+    @Column(nullable = false)
+    private String expirationDate;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
