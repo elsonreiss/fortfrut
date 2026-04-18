d159bbc5db466b34159f549d4859fa37dc0d26b8
Elson Reis <elsonreis084@gmail.com>
2026-04-18 16:13:29 -0300

    feat(category): add REST endpoints

--- src/main/java/fortfrut/controller/CategoryController.java
+++ src/main/java/fortfrut/controller/CategoryController.java
@@ -1,4 +1,45 @@
 package fortfrut.controller;
 
+import fortfrut.entity.Category;
+import fortfrut.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+@CrossOrigin
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(category));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAllCategories() {
+        return ResponseEntity.ok(categoryService.findAllCategories());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findCategoryById(@PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.findCategoryById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id,@RequestBody Category updatedCategory) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, updatedCategory));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
 }
