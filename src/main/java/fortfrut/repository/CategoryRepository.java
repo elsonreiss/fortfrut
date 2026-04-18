d4f1d0465b2cf1f9fda6d9beb1c9858afaa8b143
Elson Reis <elsonreis084@gmail.com>
2026-04-18 16:13:00 -0300

    feat(category): add repository

--- src/main/java/fortfrut/repository/CategoryRepository.java
+++ src/main/java/fortfrut/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package fortfrut.repository;
 
-public interface CategoryRepository {
+import fortfrut.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
