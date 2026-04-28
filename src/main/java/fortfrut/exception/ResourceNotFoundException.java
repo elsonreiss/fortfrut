0e7403c03b19d84b50ab6104d01d7202bfc05434
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2026-04-28 15:22:42 -0300

    feat: Criação de tratamento de error de recurso não encontrado

---
+++ src/main/java/fortfrut/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package fortfrut.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
